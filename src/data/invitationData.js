9b47d862d89bcf87fd9b08928de18f65dbb35471
joseph imanuel setiawan <128193636+josephthewarrior1@users.noreply.github.com>
2025-06-27 09:10:15 +0700

    ubah path gambar

--- src/data/invitationData.js
+++ src/data/invitationData.js
@@ -1,31 +1,32 @@
 // invitationData.js
 import { generateCalendarLink } from './utils';
+const base = import.meta.env.BASE_URL;
 
 const invitationData = {
   coupleName: "Teosaner & Sherin",
   groom: "TEOSANER YUTANESY IMAN",
   bride: "SHERIN ANGELA",
-  backgroundImage: "/assets/dq.jpg",
-  backgroundImage2: "/assets/dq2.jpg",
-  backgroundImageGroom: "/assets/groom.jpg",
-  backgroundImageBride: "/assets/bride.jpg",
-  backgroundImageLive: "/assets/live.jpg",
-  backgroundDesign: "/assets/floral.jpg",
-  audioUrl: "/assets/music.mp3",
+  backgroundImage: `${base}assets/dq.jpg`,
+  backgroundImage2: `${base}assets/dq2.jpg`,
+  backgroundImageGroom: `${base}assets/groom.jpg`,
+  backgroundImageBride: `${base}assets/bride.jpg`,
+  backgroundImageLive: `${base}assets/live.jpg`,
+  backgroundDesign: `${base}assets/floral.jpg`,
+  audioUrl: `${base}assets/music.mp3`,
   date: "5 Januari 2025",
   time: "10.00 - 12.00 WIB",
   location: "Hotel Grand Indonesia",
   locationAddress: "Gereja Sidang Jemaat Kristus\nJl. Raya Darmo Permai III\nSurabaya, Jawa Timur\n60239",
-  dateTimeImage: "/assets/nikah.jpg",
+  dateTimeImage: `${base}assets/nikah.jpg`,
   mapsLink: "https://maps.app.goo.gl/QAcpiFfeWeyWbKsbA",
   zoomLink: "https://zoom.us/your-meeting-link",
   youtubeLink: "https://youtube.com/your-livestream-link",
   galleryImages: [
-    "/assets/1.jpg",
-    "/assets/2.jpg",
-    "/assets/3.jpg",
-    "/assets/4.jpg",
-    "/assets/5.jpg"
+    `${base}assets/1.jpg`,
+    `${base}assets/2.jpg`,
+    `${base}assets/3.jpg`,
+    `${base}assets/4.jpg`,
+    `${base}assets/5.jpg`
   ],
   get calendarLink() {
     return generateCalendarLink({
