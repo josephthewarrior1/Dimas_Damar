ed33c4aba44f469469e431cc28e7c707f5116aec
joseph imanuel setiawan <128193636+josephthewarrior1@users.noreply.github.com>
2025-06-28 12:46:58 +0700

    fix bisa

--- src/data/assetPath.js
+++ src/data/assetPath.js
@@ -1,8 +1,3 @@
 export function assetPath(path) {
-    // Untuk development
-    if (import.meta.env.DEV) {
-      return `/assets/${path}`;
-    }
-    // Untuk production (GitHub Pages)
-    return `/wedding_invitation/assets/${path}`;
-  }
+  return `/assets/${path}`;
+}
