0f317b780d0dbe75860e670508f2b9b3c9d3df15
joseph imanuel setiawan <128193636+josephthewarrior1@users.noreply.github.com>
2025-06-28 12:00:16 +0700

    bisa fix bisa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/wedding_invitation/', // 🔥 Ini kunci
+  base: '/wedding_invitation/',
   server: {
     port: 3000,
     open: true
